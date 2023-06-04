a6ba79455636707ca669ec0e1891b337f41bff6d
nielass <none>
2023-06-04 21:29:08 +0200

    add fzf into telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,6 +40,10 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use { 
+    'nvim-telescope/telescope-fzf-native.nvim', 
+    run = 'make',
+  }
 
   -- cmp completion and snipping
   use {
