7df3e2ffb18bac94fae1f574474136a05c4fd334
Nielass <jdchen_97@outlook.com>
2023-06-08 10:00:45 +0200

    reorder init

--- init.lua
+++ init.lua
@@ -2,19 +2,20 @@
 require("plugins")
 
 require("basic")
-require("theme")
 require("keybinding")
+require("theme")
 
-require("plugin-config/null_ls")
-require("plugin-config/telescope")
-require("plugin-config/treesitter")
 require("plugin-config/bufferline")
 require("plugin-config/comment")
+require("plugin-config/gitsigns")
 require("plugin-config/lualine")
+require("plugin-config/null_ls")
 require("plugin-config/nvim_cmp")
 require("plugin-config/nvim_tree")
-require("plugin-config/gitsigns")
+require("plugin-config/telescope")
+require("plugin-config/treesitter")
 
+-- dont change following order
 require("lsp/mason")
 require("lsp/lsp_setup")
 
