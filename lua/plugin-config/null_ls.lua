f975f4ceabcc1b991c40ca94699760e6d9ada600
Nielass <jdchen_97@outlook.com>
2023-06-23 13:18:10 +0200

    default disable pylint

--- lua/plugin-config/null_ls.lua
+++ lua/plugin-config/null_ls.lua
@@ -2,7 +2,7 @@ local null_ls = require("null-ls")
 
 local null_ls_setup = {
     sources = {
-        null_ls.builtins.diagnostics.pylint,
+        -- null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.clang_format.with({
             extra_args = {
