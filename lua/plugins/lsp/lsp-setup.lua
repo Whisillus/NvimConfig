4b919e7c7e80fc7b5f83fd66161a6a4bd6e231b7
Nielass <jdchen_97@outlook.com>
2023-10-01 14:12:54 +0200

    add texlab and pyright

--- lua/plugins/lsp/lsp-setup.lua
+++ lua/plugins/lsp/lsp-setup.lua
@@ -8,13 +8,15 @@ local clangd = {
       "hrsh7th/cmp-nvim-lsp",
     },
 
-    ft = { "c", "cpp", "cuda" },
+    lazy = false,
     config = function(_, opts) 
+        -- Setup Clangd
         local cmp = require('cmp_nvim_lsp').default_capabilities()
         require('lspconfig').clangd.setup({
             capabilities = cmp,
             cmd = {
                 "clangd",
+                "--offset-encoding=utf-16",
                 -- "--all-scopes-completion",
                 -- "--suggest-missing-includes",
                 -- "--background-index",
@@ -24,13 +26,21 @@ local clangd = {
                 -- "--completion-style=detailed",
                 -- "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
                 -- "--clang-tidy",
-                "--offset-encoding=utf-16",
                 -- "--clang-tidy-checks=-*,llvm-*,clang-analyzer-*,modernize-*,-modernize-use-trailing-return-type",
                 -- "--fallback-style=Google",
                 -- "--header-insertion=never",
                 -- "--query-driver=<list-of-white-listed-complers>"
             }
         })
+
+        -- Setup texlab
+        require('lspconfig').texlab.setup({
+        })
+
+        -- Setup texlab
+        require('lspconfig').pyright.setup({
+        })
+
     end
 }
 
