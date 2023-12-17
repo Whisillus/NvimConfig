d5f2ec6fa4f48bc7c9b95aa1adaa10ea5c0cf281
Whisillus <whisillus@gmail.com>
2023-12-17 20:33:31 +0100

    remove keymap f and F

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -183,25 +183,25 @@ map("n", "<F9>", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 
-vim.keymap.set('', 'f', 
-    function()
-        hop.hint_char1({ 
-            direction = directions.AFTER_CURSOR,
-            current_line_only = true 
-        })
-    end, 
-    {remap=true}
-    )
-
-vim.keymap.set('', 'F', 
-    function()
-        hop.hint_char1({ 
-            direction = directions.BEFORE_CURSOR,
-            current_line_only = true 
-        })
-    end, 
-    {remap=true}
-    )
+-- vim.keymap.set('', 'f', 
+--     function()
+--         hop.hint_char1({ 
+--             direction = directions.AFTER_CURSOR,
+--             current_line_only = true 
+--         })
+--     end, 
+--     {remap=true}
+--     )
+--
+-- vim.keymap.set('', 'F', 
+--     function()
+--         hop.hint_char1({ 
+--             direction = directions.BEFORE_CURSOR,
+--             current_line_only = true 
+--         })
+--     end, 
+--     {remap=true}
+--     )
 
 vim.keymap.set('', 't', 
     function()
