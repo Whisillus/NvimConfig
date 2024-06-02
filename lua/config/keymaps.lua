bd9a1beed9573190344513ad8e3e9a2e8e151182
whisillus <whisillus@gmail.com>
2024-06-02 16:56:40 +0200

    add back diagnostic and hover glance dont support them

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -96,10 +96,10 @@ map("n", "<leader>s=", "<C-w>=", opt)
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 
--- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
--- vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', 'g]', vim.diagnostic.goto_next)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev)
+vim.keymap.set('n', 'g]', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -118,8 +118,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
     -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 
-    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 
     -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
     -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
@@ -128,7 +128,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- end, opts)
 
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
 
     -- vim.keymap.set('n', '<leader>f', function()
     --   vim.lsp.buf.format { async = true }
